2fb1dfa167dac3fd0e463dc3ff62152d545dd3ad
w1zard <76121760+zxc-w1zard@users.noreply.github.com>
2022-07-13 20:27:50 +0300

    Create message_server_TCP.py
    TCP also delivers messages, but at the same time guarantees that the packet will reach safe and sound

---
+++ message_server_TCP.py
@@ -0,0 +1,13 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('192.168.1.70', 8888))
+s.listen(5)
+while True:
+    try:
+        client, addr = s.accept()
+    except KeyboardInterrupt:
+        s.close()
+        break
+    else:
+        result = client.recv(1024)
+        print('Message: ', result.decode('utf-8'))
