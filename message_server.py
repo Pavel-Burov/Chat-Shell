1e24716d0ee3052334bf5971e001cd672c3910ac
Pavel Burov <76121760+Pavel-Burov@users.noreply.github.com>
2023-02-27 18:42:28 +0300

    Update message_server.py

--- message_server.py
+++ message_server.py
@@ -1,7 +1,7 @@
 # Server
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('192.168.1.70', 8888))
+s.bind(('server ip', 8888))
 result = s.recv(1024)
 print('Message:', result.decode('utf-8'))
 s.close()
@@ -9,5 +9,5 @@ s.close()
 """
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(b'Hello Nigger', ('93.181.251.115',8888))
+s.sendto(b'Message', ('client ip',8888))
 """
